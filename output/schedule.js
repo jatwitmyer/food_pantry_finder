31562d05e5068ec6100b67d2fca6370962c6a455
Jessica Twitmyer <jatwitmyer@gmail.com>
2024-04-17 13:37:16 -0400

    changes the output from a list to separate divs. removes unwanted caharacters from the hours section

--- output/schedule.js
+++ output/schedule.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const jsdom = require('jsdom');
 const { table } = require('console');
+const { unescape } = require('querystring');
 const { JSDOM } = jsdom;
 
 const data = [];
@@ -24,44 +25,54 @@ fs.createReadStream('../pantries.csv')
         data.forEach((row) => { //for each row from the csv
             //find the hours for that day and put it in the table if they're not closed
             const allHours = row.Hours
-            const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-            days.forEach((day) => {
-              const dayIndex = allHours.indexOf(day);
-              if (dayIndex !== -1) {
-                  const semicolonIndex = allHours.indexOf(';', dayIndex);
-                  const listedHours = allHours.slice(dayIndex, semicolonIndex);
+            if (row.Hours === "") {
+              console.log("No hours found for this pantry")
+            }
+            else {
 
+              const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+              days.forEach((day) => {
+                const dayIndex = allHours.indexOf(day);
+                if (dayIndex !== -1) {
+                  const semicolonIndex = allHours.indexOf(';', dayIndex);
+                  let listedHours = allHours.slice(dayIndex, semicolonIndex);
+                  listedHours = listedHours.replace(". Hide open hours for the wee", '');
+                  
                   if (!listedHours.includes('Closed')) {
-                      const dayCell = tableData.querySelector(`.${day}`).querySelector('ul');
-                      const li = document.createElement('li');
-                      console.log("li made")
-            
-                      const hoursSpan = document.createElement('span').textContent = `${listedHours.slice(day.length + 2)}: `;
-                      li.append(hoursSpan);
-            
-                      const nameSpan = document.createElement('span').textContent = row.Name;
-                      li.append(nameSpan);
-            
-                      const addressP = document.createElement('p').textContent = `Address: ${row.Address}`;
-                      li.append(addressP);
-            
-                      const phoneP = document.createElement('p').textContent = `Phone: ${row.Phone}`;
-                      li.append(phoneP);
-            
-                      const websiteP = document.createElement('p').textContent = `Website: ${row.Website}`;
-                      li.append(websiteP);
-            
-                      const ratingP = document.createElement('p').textContent = `Rating: ${row.Rating}`;
-                      li.append(ratingP);
-            
-                      console.log("li populated")
-                      dayCell.append(li);
-                      console.log("li appended")
+                    const dayCell = tableData.querySelector(`.${day}`);
+                    const div = document.createElement('div');
+                    div.className = "pantry-card";
+                    console.log("div made")
+                    
+                    const hoursSpan = document.createElement('span').textContent = `${listedHours.slice(day.length + 2)}: `;
+                    hoursSpan.className = "hours";
+                    div.append(hoursSpan);
+                    
+                    const nameSpan = document.createElement('span').textContent = row.Name;
+                    nameSpan.className = "name";
+                    div.append(nameSpan);
+                    
+                    const addressP = document.createElement('p').textContent = `Address: ${row.Address}`;
+                    addressP.className = "address";
+                    div.append(addressP);
+                    
+                    const phoneP = document.createElement('p').textContent = `Phone: ${row.Phone}`;
+                    phoneP
+                    div.append(phoneP);
+                    
+                    const websiteP = document.createElement('p').textContent = `Website: ${row.Website}`;
+                    websiteP.className = "website";
+                    div.append(websiteP);
+                    
+                    const ratingP = document.createElement('p').textContent = `Rating: ${row.Rating}`;
+                    ratingP.className = "rating";
+                    div.append(ratingP);
+                    
+                    dayCell.append(div);
                   }
-              } else {
-                  // console.log(`The day "${day}" was not found.`);
-              }
-            });
+                } 
+              });
+            }
 
         })
 
