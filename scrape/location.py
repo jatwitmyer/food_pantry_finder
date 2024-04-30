e9a25fe51c8f0624a98eb0cfe140cb0c23476027
Jessica Twitmyer <jatwitmyer@gmail.com>
2024-04-29 21:48:20 -0400

    soup burned. being remade

--- scrape/location.py
+++ scrape/location.py
@@ -1,10 +1,33 @@
 import  mechanicalsoup
+from bs4 import BeautifulSoup
 
 
 browser = mechanicalsoup.StatefulBrowser()
 browser.open("https://www.latlong.net/convert-address-to-lat-long.html")
-# print(browser.page)
 
-browser.select_form("form[id='Ke1721']")
+# Assuming the input has an class attribute
+input_class = "width70"
 
+# Get the form
+form = browser.select_form()
 browser.form.print_summary()
+
+# browser["input"] = "1600 Amphitheatre Parkway, Mountain View, CA"
+browser.form.set_input({"class": input_class}, "1600 Amphitheatre Parkway, Mountain View, CA")
+# browser.form.print_summary()
+
+# # Get the BeautifulSoup representation of the form
+# soup_form = BeautifulSoup(str(form.form), 'html.parser')
+
+# # Find the input element
+# input_element = soup_form.find('input', {'class': input_class})
+
+# # Check if the input element was found
+# if input_element:
+#     # Set the value of the input element
+#     input_element['value'] = "1600 Amphitheatre Parkway, Mountain View, CA"
+#     print(input_element)
+#     # browser.launch_browser()
+#     browser.form.print_summary()
+# else:
+#     print(f"No input element with id '{input_class}' found")
