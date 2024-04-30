fc38397203d4db7645282f7a7dd358a3b0969d79
Jessica Twitmyer <jatwitmyer@gmail.com>
2024-04-29 23:47:17 -0400

    should work. can't test because query limit reached

--- scrape/location.py
+++ scrape/location.py
@@ -2,18 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert 
 import time
+import csv
+
+# Get the address from the user
+country = input("Please enter a country: ")
+zip_code = input("Please enter a zip code: ")
 
 driver = webdriver.Chrome()
-try: 
+
+try:
     driver.get("https://www.latlong.net/convert-address-to-lat-long.html")
-    driver.find_element(By.CLASS_NAME, 'width70').send_keys("United States, 33647")
+    driver.find_element(By.CLASS_NAME, 'width70').send_keys(f"{country}, {zip_code}")
     driver.find_element(By.XPATH, '//*[@title="Find lat long coordinates"]').click()
-    time.sleep(1)
     while driver.find_element(By.ID, 'loading').is_displayed():
         time.sleep(1)
     latLong = driver.find_element(By.ID, 'latlngspan').get_attribute('innerHTML')
     print(latLong)
-    # print(driver.page_source)
+
+    # Write the latLong variable to a CSV file
+    with open('scrape/latlong.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        # writer.writerow(["LatLong"])
+        writer.writerow([latLong])
 except Exception:
     print("Exception Occurs")
 
--- scrape/main.js
+++ scrape/main.js
@@ -3,10 +3,30 @@
 const{chromium} = require('playwright');
 const fs = require('fs');
 
+async function fetchLocation() {
+  fs.readFile('scrape/latlong.csv', 'utf8', (err, location) => {
+    if (err) {
+      console.error('Error while reading latlong.csv:', err);
+    }
+    else {
+      console.log('File read successfully');
+      console.log(location);
+      location = location.replace("(", "");
+      location = location.replace(")", "");
+      location = location.replaceAll('"', "");
+      location = location.replace(/\s+/g, '')
+      console.log(location);
+      googleUrl = `https://www.google.com/maps/search/food+pantries/@${location},12z?entry=ttu`;
+      return googleUrl
+    }
+  });
+}
+
 (async () => {
+  googleUrl = await fetchLocation();
   fs.writeFile('scrape/pantries.csv', '', err => {
     if (err) {
-      console.error('Error while clearing the file:', err);
+      console.error('Error while clearing pantries.csv:', err);
     } else {
       console.log('File cleared successfully');
     }
@@ -16,124 +36,131 @@ const fs = require('fs');
   // googleUrl ='https://www.google.com/maps/search/food+pantries/@28.1447894,-82.4283315,12z/data=!3m1!4b1?entry=ttu' //tampa
   // googleUrl ='https://www.google.com/maps/search/food+pantries/@39.742043,-104.991531,12z?entry=ttu' //denver
   // googleUrl ='https://www.google.com/maps/search/food+pantries/@39.952583,-75.165222,12z?entry=ttu' //philadephia
-  googleUrl ='https://www.google.com/maps/search/food+pantries/@29.6636297,-82.3576781,12z?entry=ttu' //gainesville
+  // googleUrl ='https://www.google.com/maps/search/food+pantries/@29.6636297,-82.3576781,12z?entry=ttu' //gainesville
+  // console.log(googleUrl)
 
   console.time("Execution Time");
 
   const browser = await chromium.launch({headless:true});
+  if (browser) {
+    console.log('Browser opened.');
+  }
   const context = await browser.newContext();
   context.setDefaultTimeout(10000); // Set default timeout to 10 seconds
   const page = await browser.newPage();
+  if (page) {
+    console.log(googleUrl)
+  }
   await page.goto(googleUrl);
   await page.waitForSelector('[jstcache="3"]');
 
-  // const scrollable = await page.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]');
-  // if (!scrollable) {
-  //   console.log('Scrollable element not found.');
-  //   await browser.close();
-  //   return;
-  // }
+  // // const scrollable = await page.$('xpath=/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]');
+  // // if (!scrollable) {
+  // //   console.log('Scrollable element not found.');
+  // //   await browser.close();
+  // //   return;
+  // // }
 
-  // let endOfList=false;
-  // while (!endOfList) {
-  //   await scrollable.evaluate(node => node.scrollBy(0,50000));
-  //   endOfList = await page.evaluate(()=>document.body.innerText.includes("You've reached the end of the list"));
-  // }
+  // // let endOfList=false;
+  // // while (!endOfList) {
+  // //   await scrollable.evaluate(node => node.scrollBy(0,50000));
+  // //   endOfList = await page.evaluate(()=>document.body.innerText.includes("You've reached the end of the list"));
+  // // }
 
   
-  const urlsLocator = page.locator('a');
-  const urls = await urlsLocator.evaluateAll(links => links.map(link => link.href).filter(href => href.startsWith('https://www.google.com/maps/place/')));
+  // const urlsLocator = page.locator('a');
+  // const urls = await urlsLocator.evaluateAll(links => links.map(link => link.href).filter(href => href.startsWith('https://www.google.com/maps/place/')));
 
-  const scrapePageData = async(url) => {
-    const newPage = await browser.newPage();
-    await newPage.goto(url);
-    await newPage.waitForSelector('[jstcache="3"]');
+  // const scrapePageData = async(url) => {
+  //   const newPage = await browser.newPage();
+  //   await newPage.goto(url);
+  //   await newPage.waitForSelector('[jstcache="3"]');
 
-    let name = '';
-    try {
-      const nameElement = await newPage.locator('[class="DUwDvf lfPIob"]')
-      name = nameElement ? await nameElement.evaluate(element => element.textContent) : '';
-      name = `"${name}"`;
-      console.log(name);
-    }
-    catch {
-      console.log(`Error finding name: ${url}`);
-    }
+  //   let name = '';
+  //   try {
+  //     const nameElement = await newPage.locator('[class="DUwDvf lfPIob"]')
+  //     name = nameElement ? await nameElement.evaluate(element => element.textContent) : '';
+  //     name = `"${name}"`;
+  //     console.log(name);
+  //   }
+  //   catch {
+  //     console.log(`Error finding name: ${url}`);
+  //   }
     
-    let address = '';
-    try {
-    const addressElement = await newPage.locator('[data-item-id="address"]')
-    address = addressElement ? await addressElement.evaluate(element => element.textContent.slice(1)) : '';
-    address = `"${address}"`;
-    console.log(`Address: ${address}`);
-    }
-    catch {
-      console.log(`Error finding address: ${url}`);
-    }
+  //   let address = '';
+  //   try {
+  //   const addressElement = await newPage.locator('[data-item-id="address"]')
+  //   address = addressElement ? await addressElement.evaluate(element => element.textContent.slice(1)) : '';
+  //   address = `"${address}"`;
+  //   console.log(`Address: ${address}`);
+  //   }
+  //   catch {
+  //     console.log(`Error finding address: ${url}`);
+  //   }
 
-    let hours = '';
-    try {
-      const hoursElement = await newPage.locator('div.t39EBf.GUrTXd')
-      hours = hoursElement ? await hoursElement.evaluate(element => element.ariaLabel) : '';
-      hours = `"${hours}"`;
-      console.log(hours);
-    }
-    catch {
-      console.log(`Error finding hours: ${url}`);
-    }
+  //   let hours = '';
+  //   try {
+  //     const hoursElement = await newPage.locator('div.t39EBf.GUrTXd')
+  //     hours = hoursElement ? await hoursElement.evaluate(element => element.ariaLabel) : '';
+  //     hours = `"${hours}"`;
+  //     console.log(hours);
+  //   }
+  //   catch {
+  //     console.log(`Error finding hours: ${url}`);
+  //   }
 
-    let phone = '';
-    try {
-    const phoneElement = await newPage.locator('[data-tooltip="Copy phone number"]').first()
-    phone = phoneElement ? await phoneElement.evaluate(element => element.textContent.slice(1)) : '';
-    phone = `"${phone}"`;
-    console.log(`Phone: ${phone}`);
-    }
-    catch {
-      console.log(`Error finding phone: ${url}`);
-    }
+  //   let phone = '';
+  //   try {
+  //   const phoneElement = await newPage.locator('[data-tooltip="Copy phone number"]').first()
+  //   phone = phoneElement ? await phoneElement.evaluate(element => element.textContent.slice(1)) : '';
+  //   phone = `"${phone}"`;
+  //   console.log(`Phone: ${phone}`);
+  //   }
+  //   catch {
+  //     console.log(`Error finding phone: ${url}`);
+  //   }
 
-    let website = '';
-    try {
-    const websiteElement = await newPage.locator('[data-tooltip="Open website"]').first()
-    website = websiteElement ? await websiteElement.evaluate(element => element.href) : '';
-    website = `"${website}"`;
-    console.log(`Website: ${website}`);
-    }
-    catch {
-      console.log(`Error finding website: ${url}`);
-    }
+  //   let website = '';
+  //   try {
+  //   const websiteElement = await newPage.locator('[data-tooltip="Open website"]').first()
+  //   website = websiteElement ? await websiteElement.evaluate(element => element.href) : '';
+  //   website = `"${website}"`;
+  //   console.log(`Website: ${website}`);
+  //   }
+  //   catch {
+  //     console.log(`Error finding website: ${url}`);
+  //   }
 
-    let rating = '';
-    try {
-    const ratingElement = await newPage.locator('span.ceNzKf[aria-label]').first()
-    rating = ratingElement ? await ratingElement.evaluate(element => element.ariaLabel.slice(0,9)) : '';
-    rating = `"${rating}"`;
-    console.log(`Rating: ${rating}`);
-    }
-    catch {
-      console.log(`Error finding rating: ${url}`);
-    }
+  //   let rating = '';
+  //   try {
+  //   const ratingElement = await newPage.locator('span.ceNzKf[aria-label]').first()
+  //   rating = ratingElement ? await ratingElement.evaluate(element => element.ariaLabel.slice(0,9)) : '';
+  //   rating = `"${rating}"`;
+  //   console.log(`Rating: ${rating}`);
+  //   }
+  //   catch {
+  //     console.log(`Error finding rating: ${url}`);
+  //   }
     
-    url=`"${url}"`;
+  //   url=`"${url}"`;
 
-    await newPage.close();
-    return{name,address,hours,phone,website,rating,url};
-  };
+  //   await newPage.close();
+  //   return{name,address,hours,phone,website,rating,url};
+  // };
 
-  const batchSize=5;
-  const results=[];
-  for(let i=0; i<urls.length; i+=batchSize) {
-    const batchUrls = urls.slice(i,i+batchSize);
-    const batchResults = await Promise.all(batchUrls.map(url => scrapePageData(url))); //times out on coop's computer
-    results.push(...batchResults);
-    console.log(`Batch ${i/batchSize+1} completed.`);
-  }
+  // const batchSize=5;
+  // const results=[];
+  // for(let i=0; i<urls.length; i+=batchSize) {
+  //   const batchUrls = urls.slice(i,i+batchSize);
+  //   const batchResults = await Promise.all(batchUrls.map(url => scrapePageData(url))); //times out on coop's computer
+  //   results.push(...batchResults);
+  //   console.log(`Batch ${i/batchSize+1} completed.`);
+  // }
 
-  const csvHeader='Name,Address,Hours,Phone,Website,Rating,Url\n';
-  const csvRows=results.map(r=>`${r.name},${r.address},${r.hours},${r.phone},${r.website},${r.rating},${r.url}`).join('\n');
-  fs.writeFileSync(nameSheet,csvHeader+csvRows);
+  // const csvHeader='Name,Address,Hours,Phone,Website,Rating,Url\n';
+  // const csvRows=results.map(r=>`${r.name},${r.address},${r.hours},${r.phone},${r.website},${r.rating},${r.url}`).join('\n');
+  // fs.writeFileSync(nameSheet,csvHeader+csvRows);
   
   await browser.close();
-  console.timeEnd("Execution Time");
+  // console.timeEnd("Execution Time");
 })();
