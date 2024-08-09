c61fff7a84772cb13cdb98f3b1f1b31c9f6c1c9d
Jessica Twitmyer <jatwitmyer@gmail.com>
2024-08-08 23:19:02 -0400

    initializes css grid

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App.js';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
